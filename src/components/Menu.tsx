145ddb870e14d76871ec73a089f7861cf5ab2320
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-09-05 12:57:13 +0330

    Menu components created

---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+function Menu() {
+    return (
+        <nav className="bg-white py-3 px-8">
+            <h1 className="text-2xl font-black">تودو</h1>
+        </nav>
+    )
+}
+
+export default Menu
