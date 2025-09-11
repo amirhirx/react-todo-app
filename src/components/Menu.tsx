6040c7d068693e7a000f7584f2f7764668788381
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-09-11 10:13:21 +0330

    Add a button to menu for opening add task modal

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,19 @@
-function Menu() {
+import type { MouseEventHandler } from "react"
+
+interface IMenu {
+    toggleShowAddTaskModal: MouseEventHandler
+}
+
+function Menu({ toggleShowAddTaskModal }: IMenu) {
     return (
-        <nav className="bg-white py-3 px-8">
+        <nav className="bg-white py-3 px-8 flex items-center justify-between">
             <h1 className="text-2xl font-black">تودو</h1>
+            <button
+                onClick={toggleShowAddTaskModal}
+                className="bg-blue-400 text-white font-bold py-2 px-4 rounded-md transition active:scale-95 active:bg-blue-500"
+            >
+                کار جدید
+            </button>
         </nav>
     )
 }
