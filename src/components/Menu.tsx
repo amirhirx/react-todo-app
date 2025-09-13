eaadc2b16ca300902ea5e5bbeed3bc523e34f353
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-09-13 09:08:43 +0330

    Add plus icon in button for menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from "@heroicons/react/24/outline"
 import type { MouseEventHandler } from "react"
 
 interface IMenu {
@@ -10,9 +11,10 @@ function Menu({ toggleShowAddTaskModal }: IMenu) {
             <h1 className="text-2xl font-black">تودو</h1>
             <button
                 onClick={toggleShowAddTaskModal}
-                className="bg-blue-400 text-white font-bold py-2 px-4 rounded-md transition active:scale-95 active:bg-blue-500"
+                className="bg-blue-400 text-white font-bold py-2 px-4 rounded-md transition active:scale-95 active:bg-blue-500 flex items-center"
             >
                 کار جدید
+                <PlusIcon className="h-6 w-6 text-white mr-2" />
             </button>
         </nav>
     )
