d3d645a7d1cdaf255f8610719f38bca9339b7d40
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-09-18 22:01:13 +0330

    Open and close add new task modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import Menu from "./components/Menu"
+import AddTaskModal from "./components/AddTaskModal"
 
 function App() {
+    const [showNewTaskModal, setShowNewModal] = useState(false)
+
+    const openNewTaskModal = () => setShowNewModal(true)
+    const closeNewTaskModal = () => setShowNewModal(false)
     return (
         <>
-            <Menu />
+            <Menu toggleShowAddTaskModal={openNewTaskModal} />
+            {showNewTaskModal ? (
+                <AddTaskModal closeModal={closeNewTaskModal} />
+            ) : null}
         </>
     )
 }
