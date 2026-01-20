043008524163462679908d28b656aa7d6b7cc33b
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-01-20 21:52:16 +0330

    Update ITask import & container width in desktop view

--- src/components/TasksContainer.tsx
+++ src/components/TasksContainer.tsx
@@ -1,9 +1,9 @@
-import type { ITask } from "./Task"
+import type { ITask } from "../types/task"
 import Task from "./Task"
 
 export default function TasksContainer({ tasks }: { tasks: ITask[] }) {
     return (
-        <div className="w-11/12 md:w-2/3 mx-auto">
+        <div className="w-11/12 md:w-7/12 mx-auto">
             {tasks.map(({ id, flag, title, text }) => {
                 return (
                     <Task
